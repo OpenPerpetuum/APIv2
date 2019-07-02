27bcd435cb7a39dad52058680908243ae659bd58
mikejeffers <mike.s.jeffers@gmail.com>
2019-07-02 16:08:56 -0400

    Server stats, mining log

---
+++ src/Database/game/Procedures/API.ServerStatMining.sql
@@ -0,0 +1,31 @@
+USE [perpetuumsa]
+GO
+
+
+----------------------------------------------------
+--Server Statistics: Mining
+--Displays ores gathered as aggregate statistics grouped by ore definition
+--Last updated: 2019/07/02
+----------------------------------------------------
+
+IF OBJECT_ID('API.ServerStatMining', 'P') IS NOT NULL
+	DROP PROCEDURE API.ServerStatMining;
+GO
+
+CREATE procedure API.ServerStatMining
+	@startTime DATETIME,
+	@endTime DATETIME,
+	@pageNum INT,
+	@pageSize INT
+as
+
+SELECT mine.definition, e.definitionname, SUM(amount) AS total
+FROM [perpetuumsa].[dbo].[mininglog]  mine
+JOIN entitydefaults e ON e.definition = mine.definition
+WHERE eventtime BETWEEN @startTime AND @endTime
+GROUP BY mine.definition, e.definitionname
+ORDER BY total DESC
+OFFSET @pageSize * (@pageNum - 1) ROWS
+FETCH NEXT @pageSize ROWS ONLY;
+
+GO
